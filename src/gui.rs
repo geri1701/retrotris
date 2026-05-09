a674146445eebfc15fab0cdc019090790e2724e7
Artem Ageev <artem.v.ageev@gmail.com>
2026-05-09 22:32:36 +0300

    fix build for wondows

--- src/gui.rs
+++ src/gui.rs
@@ -36,7 +36,23 @@ pub struct Settings {
 
 impl Settings {
     pub fn config(&self) -> Window {
-        set_theme(0);
+        app::set_background_color(238, 232, 213);
+        app::set_background2_color(253, 246, 227);
+        app::set_foreground_color(7, 54, 66);
+        app::set_selection_color(203, 75, 22);
+        app::set_inactive_color(181, 137, 0);
+        Tooltip::set_color(Color::Background2);
+        Tooltip::set_text_color(Color::Foreground);
+        for (color, (r, g, b)) in [
+            (Color::Red, (220, 50, 47)),
+            (Color::Magenta, (211, 54, 130)),
+            (Color::Blue, (38, 139, 210)),
+            (Color::Cyan, (42, 161, 152)),
+            (Color::Green, (133, 153, 0)),
+        ] {
+            app::set_color(color, r, g, b);
+        }
+        app::set_visible_focus(false);
         app::set_font(self.font.unwrap_or(Font::CourierBold));
         app::set_font_size(self.font_size.unwrap_or(14));
         let (w, h) = self.size.unwrap_or((360, 640));
@@ -63,8 +79,12 @@ where
     fn update(&mut self, dt: f32);
     fn connect(window: &mut Window) {
         let path = format!(
-            "{}/.config/{}",
-            std::env::var("HOME").unwrap(),
+            "{}/.{}",
+            std::env::var(match cfg!(target_os = "windows") {
+                true => "HOMEPATH",
+                false => "HOME",
+            })
+            .unwrap(),
             window.xclass().unwrap(),
         );
         let state = std::rc::Rc::new(std::cell::RefCell::new(Self::default()));
@@ -91,6 +111,7 @@ where
         });
     }
     fn run(settings: Settings) -> Result<(), FltkError> {
+        let app = app::App::default().load_system_fonts();
         let mut window = settings.config();
         Self::connect(&mut window);
         const TICK: f64 = 0.02;
@@ -98,76 +119,8 @@ where
             window.redraw();
             app::repeat_timeout3(TICK, handle);
         });
-        app::App::default().run()
+        app.run()
     }
-}
-
-pub fn set_theme(theme: usize) {
-    const COLOR: [[u32; 5]; 4] = [
-        [
-            //SOLARIZED LIGHT
-            0xEEE8D5, //base2
-            0xFDF6E3, //base3
-            0x586E75, //base01
-            0xCB4B16, //orange
-            0xB58900, //yellow
-        ],
-        [
-            //SOLARIZED DARK
-            0x073642, //base02
-            0x002B36, //base03
-            0x93A1A1, //base1
-            0x268BD2, //blue
-            0x6C71C4, //violet
-        ],
-        [
-            //ADWAITA LIGHT
-            0xF6F5F4, //set_background_color
-            0xFCFCFC, //set_background2_color
-            0x323232, //set_foreground_color
-            0x6C71C4, //set_selection_color
-            0x3584E4, //set_inactive_color
-        ],
-        [
-            //ADWAITA DARK
-            0x353535, //set_background_color
-            0x303030, //set_background2_color
-            0xD6D6D6, //set_foreground_color
-            0x268BD2, //set_selection_color
-            0x15539E, //set_inactive_color
-        ],
-    ];
-    let color = COLOR[theme];
-    Tooltip::set_color(Color::Background2);
-    Tooltip::set_text_color(Color::Foreground);
-    //app::set_visible_focus(false);
-    app::set_scheme(match theme % 2 {
-        1 => app::Scheme::Gtk,
-        _ => app::Scheme::Oxy,
-    });
-    let (r, g, b) = Color::from_hex(color[0]).to_rgb();
-    app::set_background_color(r, g, b);
-    let (r, g, b) = Color::from_hex(color[1]).to_rgb();
-    app::set_background2_color(r, g, b);
-    let (r, g, b) = Color::from_hex(color[2]).to_rgb();
-    app::set_foreground_color(r, g, b);
-    let (r, g, b) = Color::from_hex(color[3]).to_rgb();
-    app::set_selection_color(r, g, b);
-    let (r, g, b) = Color::from_hex(color[4]).to_rgb();
-    app::set_inactive_color(r, g, b);
-    for (hex, color) in [
-        (0xB58900, Color::Yellow),
-        (0xDC322F, Color::Red),
-        (0xD33682, Color::Magenta),
-        (0x268BD2, Color::Blue),
-        (0x2AA198, Color::Cyan),
-        (0x859900, Color::Green),
-    ] {
-        let (r, g, b) = Color::from_hex(hex).to_rgb();
-        app::set_color(color, r, g, b);
-    }
-    app::set_visible_focus(false);
-    app::redraw();
 }
 
 pub trait Painter {
